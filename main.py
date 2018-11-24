3417953aa576f157b54cd4d24291ed00e1e303d1
Niels Balemans <balemans.niels@gmail.com>
2018-11-24 13:54:07 +0100

    added motivation generator

--- main.py
+++ main.py
@@ -6,9 +6,24 @@ app = Flask(__name__)
 
 extractor = kwe.KeyWordExtractor()
 
+
 @app.route("/getKeywords/<id>")
 def getKeywords(id):
     return json.dumps(extractor.extractKeywords(id))
+
+
+@app.route("/generateMotivation/<keyword>/<score>")
+def motivationGenerator(keyword, score):
+    motivation = {"message":""}
+    score = int(score)
+    if score < 9:
+        motivation["message"] = "Denied allowance because of good: {}".format(keyword)
+    elif score > 9 and score < 19:
+        motivation["message"] = "Granted allowance because of bad: {}".format(keyword)
+    else:
+        motivation["message"] = "Score out of bounds"
+
+    return json.dumps(motivation)
 
 
 if __name__ == '__main__':
